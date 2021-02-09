25e157d8f004980463f2ef1b494b365eb10bb7a5
nayem1999 <nayemk900@gmail.com>
2021-02-09 18:46:40 +0600

    Updated Rock Music API Site

---
+++ app.js
@@ -0,0 +1,49 @@
+const getInput = () => {
+    const inputValue = document.getElementById('searchInput').value;
+    const url = `https://api.lyrics.ovh/suggest/${inputValue}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            displaySongs(data.data);
+        })
+        .catch(error => {
+            alert("'Sorry! I failed to load lyrics, Please try again later!!!'")
+        });
+}
+
+const displaySongs = songs => {
+    const songContainer = document.getElementById('songContainer');
+    songContainer.innerText = '';
+    songs.forEach(song => {
+        const songDiv = document.createElement('div');
+        songDiv.className = 'single-result row align-items-center my-3 p-3';
+        songDiv.innerHTML = `
+        <div class="col-md-9">
+            <h3 class="lyrics-name">${song.title}</h3>
+            <p class="author lead">Album by <span>${song.artist.name}</span></p>
+        </div>
+        <audio controls>
+            <source src="${song.preview}" type="audio/ogg">
+        </audio>
+        <div class="col-md-3 text-md-right text-center">
+            <button onclick="getLyrics('${song.artist.name}','${song.title}')" class="btn btn-success">Get Lyrics</button>
+        </div>`;
+
+        songContainer.append(songDiv);
+        document.getElementById('lyrics').innerText = '';
+    });
+}
+const getLyrics = (artist, title) => {
+    const url = `https://api.lyrics.ovh/v1/${artist}/${title}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            const lyrics = document.getElementById('lyrics');
+            lyrics.innerText = data.lyrics
+        })
+        .catch(error => {
+            alert("'Sorry! I failed to load lyrics, Please try again later!!!'")
+        });
+        lyrics.innerText = '';
+
+}
